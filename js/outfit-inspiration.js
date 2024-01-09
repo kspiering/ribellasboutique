e8122ac584e36336a6c037f994da63f27413a5fe
Kim Spiering <kim.spiering@gmx.ch>
2024-01-09 20:10:26 +0100

    Korrekturen Contact / Outfit Inspo

--- js/outfit-inspiration.js
+++ js/outfit-inspiration.js
@@ -1,49 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sliderContainer = document.querySelector(".images-all");
-  const playPauseButton = document.getElementById("play");
-  const pauseButton = document.getElementById("pause");
+  if (window.innerWidth < 670) {
+    const sliderContainer = document.querySelector(".images-all");
+    const playPauseButton = document.getElementById("play");
+    const pauseButton = document.getElementById("pause");
 
-  document
-    .querySelector(".arrow-container .left")
-    .addEventListener("click", moveLeft);
+    document
+      .querySelector(".arrow-container .left")
+      .addEventListener("click", moveLeft);
 
-  document
-    .querySelector(".arrow-container .right")
-    .addEventListener("click", moveRight);
+    document
+      .querySelector(".arrow-container .right")
+      .addEventListener("click", moveRight);
 
-  let isPlaying = true;
-  let slideInterval;
+    let isPlaying = true;
+    let slideInterval;
 
-  playPauseButton.addEventListener("click", togglePlayPause);
+    playPauseButton.addEventListener("click", togglePlayPause);
 
-  function moveRight() {
-    clearInterval(slideInterval); // Stoppe die automatische Slideshow
-    sliderContainer.appendChild(document.querySelector(".slide"));
-    if (isPlaying) {
-      slideInterval = setInterval(moveRight, 3000); // Starte die automatische Slideshow wieder
+    function moveRight() {
+      clearInterval(slideInterval); // Stoppe die automatische Slideshow
+      sliderContainer.appendChild(document.querySelector(".slide"));
+      if (isPlaying) {
+        slideInterval = setInterval(moveRight, 3000); // Starte die automatische Slideshow wieder
+      }
     }
-  }
 
-  function moveLeft() {
-    clearInterval(slideInterval); // Stoppe die automatische Slideshow
-    const slides = document.querySelectorAll(".slide");
-    sliderContainer.insertBefore(slides[slides.length - 1], slides[0]);
-    if (isPlaying) {
-      slideInterval = setInterval(moveRight, 3000); // Starte die automatische Slideshow wieder
+    function moveLeft() {
+      clearInterval(slideInterval); // Stoppe die automatische Slideshow
+      const slides = document.querySelectorAll(".slide");
+      sliderContainer.insertBefore(slides[slides.length - 1], slides[0]);
+      if (isPlaying) {
+        slideInterval = setInterval(moveRight, 3000); // Starte die automatische Slideshow wieder
+      }
     }
-  }
 
-  function togglePlayPause() {
-    isPlaying = !isPlaying;
-    if (isPlaying) {
-      playPauseButton.textContent = "Pause";
-      slideInterval = setInterval(moveRight, 3000);
-    } else {
-      playPauseButton.textContent = "Play";
-      clearInterval(slideInterval);
+    function togglePlayPause() {
+      isPlaying = !isPlaying;
+      if (isPlaying) {
+        playPauseButton.textContent = "Pause";
+        slideInterval = setInterval(moveRight, 3000);
+      } else {
+        playPauseButton.textContent = "Play";
+        clearInterval(slideInterval);
+      }
     }
-  }
 
-  // Starte die Slideshow automatisch
-  slideInterval = setInterval(moveRight, 3000);
+    // Starte die Slideshow automatisch
+    slideInterval = setInterval(moveRight, 3000);
+  }
+  alert("Sie verwenden die mobile Version!");
 });
