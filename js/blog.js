a17862ee834162b6db99c28c21e6d2fe3d8ec40d
Kim Spiering <kim.spiering@gmx.ch>
2024-01-23 20:45:59 +0100

    blog

--- js/blog.js
+++ js/blog.js
@@ -3,19 +3,25 @@ async function getData() {
   const response = await fetch("/js/dummy.json");
   const data = await response.json();
   console.log(data);
+  displayPost(data);
 }
 
 function displayPost(data) {
-  data.forEach((post) => {
+  data.forEach((blogPost) => {
+    const div = document.createElement("div");
+    div.classList.add("blogPost");
     const template = `
-        <ul>
-            <li> Title</li>
-            <li> Subtitle</li>
-            <li> Location</li> <li> Date</li>
-            <li> Image</li>
-            <li> Text</li>
-            <li> Tags</li>
-        </ul>
+       
+            <p id="titel">${blogPost.title}</p>
+            <p id="subtitle"> ${blogPost.subtitle} </p>
+            <p id="location">${blogPost.location} ${blogPost.date} </p>
+            <p id="text"> ${blogPost.text} </p>
+            <img src="${blogPost.image}">
+            <p id="tags">#${blogPost.tags} </p>
+        
         `;
+
+    div.innerHTML = template;
+    document.querySelector(".blogPost").appendChild(div);
   });
 }
