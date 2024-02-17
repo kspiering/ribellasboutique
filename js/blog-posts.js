f5fb6163caac4b600d7bad20e2237015e56c808c
Kim Spiering <kim.spiering@gmx.ch>
2024-02-17 13:48:30 +0100

    blog post feedback

--- js/blog-posts.js
+++ js/blog-posts.js
@@ -1,19 +1,27 @@
 getData();
+
 async function getData() {
-  const response = await fetch("../json/dummy.json");
-  const data = await response.json();
-  console.log(data);
-  displayPost(data);
+  try {
+    const response = await fetch("../json/dummy.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    displayPost(data);
+  } catch (error) {
+    console.error("Could not fetch the data: ", error.message);
+  }
 }
 
 function displayPost(data) {
+  const container = document.querySelector(".blogPost");
   data.forEach((blogPost) => {
     const div = document.createElement("div");
     div.classList.add("blogPost");
     const template = `
        
-            <p id="titel">${blogPost.title}</p>
-            <p id="subtitle"> ${blogPost.subtitle} </p>
+            <h2 id="titel">${blogPost.title}</h2>
+            <h3 id="subtitle"> ${blogPost.subtitle} </h3>
             <p id="location">${blogPost.location} ${blogPost.date} </p>
             <img src="${blogPost.image}">
             <p id="tags">#${blogPost.tags} </p>
@@ -22,6 +30,6 @@ function displayPost(data) {
         `;
 
     div.innerHTML = template;
-    document.querySelector(".blogPost").appendChild(div);
+    container.appendChild(div);
   });
 }
